31c36d65a3fd7ab40717bdfbcc9eafc43631df81
Malia Jones <maliacatjones@gmail.com>
2023-11-10 20:50:32 +0000

    setup, minor changes to homepage, creation of MJ's page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+// website URL: https://effulgent-syrniki-2f4bcf.netlify.app/
+
 import Head from 'next/head'
 
 export default function Home() {
@@ -9,17 +11,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
+        <h1 className='title'>Homepage</h1>
 
         <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
+          Already edited <code>src/pages/index.js</code> to be our home page.
         </p>
         <p className='description2'>
           Next, each team member will create their own about me file in the
           pages directory:{' '}
         </p>
-        <code>src/page/teamMemberName.js</code>
+        <code>src/pages/teamMemberName.js</code>
 
         <div className='grid'>
           <a href='/TomHolland ' className='card'>
@@ -27,36 +28,45 @@ export default function Home() {
             <p>Click here to navigate to an example page</p>
           </a>
 
-          <a
+          {/*------------------comment using this style (cntrl+/)-----------------*/}
+          {/* Commented out the other boxes so we have room to build homepage */}
+          
+          {/* <a
             href='https://github.com/vedant29/Project-CSCI-1000'
             className='card'>
             <h3>Project Resources&rarr;</h3>
             <p>
               Return to the Github for resources on creating your own website!
             </p>
-          </a>
-          <a
+          </a> */}
+
+          {/* <a
             href='https://nextjs.org/learn/foundations/about-nextjs'
             className='card'>
             <h3>Next.js&rarr;</h3>
             <p>Click here to learn more about Next.js</p>
-          </a>
-          <a href='https://blog.hubspot.com/website/html' className='card'>
+          </a> */}
+
+          {/* <a href='https://blog.hubspot.com/website/html' className='card'>
             <h3>HTML and CSS &rarr;</h3>
             <p>Click here to learn more about HTML and CSS</p>
-          </a>{' '}
-          <a href='https://tailwindcss.com/' className='card'>
+          </a>{' '} */}
+
+          {/* <a href='https://tailwindcss.com/' className='card'>
             <h3>Tailwind css&rarr;</h3>
             <p>Click here to learn more about tailwind css</p>
-          </a>
-          <a href='https://styled-components.com/' className='card'>
+          </a> */}
+
+          {/* <a href='https://styled-components.com/' className='card'>
             <h3>Styled Components&rarr;</h3>
             <p>Click here to learn more about Styled Components</p>
-          </a>
-          <a href='https://www.netlify.com/' className='card'>
+          </a> */}
+
+          {/* <a href='https://www.netlify.com/' className='card'>
             <h3>Netlify&rarr;</h3>
             <p>Click here to learn more about Netlify to host your website</p>
-          </a>
+          </a> */}
+
         </div>
       </main>
 
