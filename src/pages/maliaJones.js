e09adfb31070f904712d2550f37cd4b9ab6e7f64
Malia Jones <maliacatjones@gmail.com>
2023-11-16 21:56:37 -0700

    recent changes to personal page

--- src/pages/maliaJones.js
+++ src/pages/maliaJones.js
@@ -6,16 +6,14 @@ export default function examplePage() {
         <title>MaliasPage</title>
         <main>
 
-            <h1>This is Malia Jones</h1>
-            <div className="row">
-                <div className="col">
-                    <img src="mjPics/portrait1.jpg"></img>
-                    <img src="mjPics/portrait2.jpg"></img>
-                    <img src="mjPics/outdoors.jpg"></img>
-                    <img src="mjPics/fish.jpg"></img>
+            <h1 className="h1">This is Malia Jones</h1>
+                <div className="row">
+                    <img src="https://i.postimg.cc/HLMd5Pb4/portrait1.jpg"></img>
+                    <img src="https://i.postimg.cc/9QKcDjwQ/portrait2.webp"></img>
+                    <img src="https://i.postimg.cc/tJhhG22p/outdoors.jpg"></img>
+                    <img src="https://i.postimg.cc/7h7NYxYv/fish.jpg"></img>
                 </div>
-            </div>
-            <p>Hi there, this is the page for Malia Jones! More info will be put here in the future; just conducting tests for now.</p>
+            <p className="p1">Hi there, this is the page for Malia Jones! More info will be put here in the future; just conducting tests for now.</p>
 
 
           <a className='button' href='/'>
@@ -26,26 +24,46 @@ export default function examplePage() {
 
         <style jsx>{`
         .row{
-            display: flex;
             flex-wrap: wrap;
             padding: 0 4px;
         }
         .col {
             flex: 50%;
             padding: 0 4px;
+            
         }
-        .col img{
+        .row img{
             margin-top: 8px;
             vertical-align = middle;
-        }`}
+            border-radius: 8px;
+            max-width: 25%;
+            height: auto;
+            margin-left: 25%;
+            margin-right: 75%;
+        }
+        .h1{
+            font-family: "Courier New", monospace;
+              text-align: center;
+        }
+        .p1{
+            font-family: "Verdana", sans-serif;
+        }
+        .button{
+            font-family: "Verdana", sans-serif;
+            text-align: center;
+        }
+        
+        
+        `}
 
     </style>
 
     <script>
-        {/* var elements = document.getElementsByClassName("col");
+        {/*var elements = maliaJones.getElementsByClassName("col");
+        var i;
         for (i = 0; i < elements.length; i++){
             elements[i].style.flex = "50%";
-        } */}
+        }*/}
     </script>
 
       </div>
